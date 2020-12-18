901ed858ec017776cd9e61a2033d1e703849c37f
Amstrong <monachelloa@uninorte.edu.co>
2020-12-17 20:19:55 -0500

    Styles in react native

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,12 +1,36 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Pressable, StyleSheet} from 'react-native';
+
 class CoinsScreen extends Component {
+  handlePress = () => {
+    console.log('Hola', this.props);
+  };
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Text>Coins Screen</Text>
+        <Pressable onPress={this.handlePress} style={styles.btn}>
+          <Text style={styles.btnText}>Ir a detail </Text>
+        </Pressable>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  btn: {
+    padding: 8,
+    backgroundColor: 'blue',
+    fontSize: 50,
+    borderRadius: 10,
+  },
+  btnText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+});
 export default CoinsScreen;
--- src/components/coins/CoinsStack.js
+++ src/components/coins/CoinsStack.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import CoinsScreen from './CoinsScreen';
+import CoinDetailScreen from './CoinDetailScreen';
 const Stack = createStackNavigator();
 const CoinsStack = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Coins" component={CoinsScreen} />
+      <Stack.Screen name="Coin Detail" component={CoinDetailScreen} />
     </Stack.Navigator>
   );
 };
