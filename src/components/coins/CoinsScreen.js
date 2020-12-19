18ed0e1dcc0d3093b59852d60b682ce47d68f384
Amstrong <monachelloa@uninorte.edu.co>
2020-12-19 10:01:03 -0500

    http methods using interfaces

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react';
 import {View, Text, Pressable, StyleSheet} from 'react-native';
-
+import Http from 'cryptoTracker/src/libs/http';
 class CoinsScreen extends Component {
+  componentDidMount = async () => {
+    const coins = await Http.instance.get(
+      'https://api.coinlore.net/api/tickers/',
+    );
+    console.log(coins);
+  };
+
   handlePress = () => {
     this.props.navigation.navigate('CoinDetail');
   };
