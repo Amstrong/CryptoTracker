461a4ca32646a8819dd54036913b2a6247c6811e
Amstrong <monachelloa@uninorte.edu.co>
2020-12-19 16:02:50 -0500

    Agregando una lista horizontal

--- src/components/coinDetail/CoinDetailScreen.js
+++ src/components/coinDetail/CoinDetailScreen.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
-import {View, Image, Text, SectionList, StyleSheet} from 'react-native';
+import {
+  View,
+  Image,
+  Text,
+  SectionList,
+  StyleSheet,
+  FlatList,
+} from 'react-native';
 import Colors from 'cryptoTracker/src/res/colors';
+import Http from '../../libs/http';
+import CoinMarketDetail from './CoinMarketDetail';
 
 class CoinDetailScreen extends Component {
   state = {
     coin: {},
+    markets: [],
   };
 
   getSymbolIcon = (name) => {
@@ -12,6 +22,12 @@ class CoinDetailScreen extends Component {
       const symbol = name.toLowerCase().replace(' ', '-');
       return `https://c1.coinlore.com/img/25x25/${symbol}.png`;
     }
+  };
+
+  getmMarkets = async (coinID) => {
+    const url = `https://api.coinlore.net/api/coin/markets/?id=${coinID}`;
+    const markets = await Http.instance.get(url);
+    this.setState({markets});
   };
 
   getSections = (coin) => {
@@ -34,10 +50,11 @@ class CoinDetailScreen extends Component {
   componentDidMount() {
     const {coin} = this.props.route.params;
     this.props.navigation.setOptions({title: coin.symbol});
+    this.getmMarkets(coin.id);
     this.setState({coin});
   }
   render() {
-    const {coin} = this.state;
+    const {coin, markets} = this.state;
     return (
       <View style={styles.container}>
         <View style={styles.subHeader}>
@@ -48,6 +65,7 @@ class CoinDetailScreen extends Component {
           <Text style={styles.titleText}>{coin.name} </Text>
         </View>
         <SectionList
+          style={styles.section}
           sections={this.getSections(coin)}
           keyExtractor={(item) => item}
           renderItem={({item}) => (
@@ -60,6 +78,15 @@ class CoinDetailScreen extends Component {
               <Text style={styles.sectionText}>{section.title}</Text>
             </View>
           )}
+        />
+
+        <Text style={styles.MarketsTitle}>Markets</Text>
+
+        <FlatList
+          style={styles.list}
+          horizontal={true}
+          data={markets}
+          renderItem={({item}) => <CoinMarketDetail item={item} />}
         />
       </View>
     );
@@ -99,6 +126,20 @@ const styles = StyleSheet.create({
   sectionText: {
     color: '#fff',
     fontSize: 14,
+    fontWeight: 'bold',
+  },
+  section: {
+    maxHeight: 220,
+  },
+  list: {
+    maxHeight: 100,
+    paddingLeft: 16,
+  },
+  MarketsTitle: {
+    color: '#fff',
+    fontSize: 16,
+    marginBottom: 16,
+    marginLeft: 16,
     fontWeight: 'bold',
   },
 });
--- src/components/coins/CoinsStack.js
+++ src/components/coins/CoinsStack.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import CoinsScreen from './CoinsScreen';
-import CoinDetailScreen from './CoinDetailScreen';
+import CoinDetailScreen from '../coinDetail/CoinDetailScreen';
 import Colors from 'cryptoTracker/src/res/colors';
 const Stack = createStackNavigator();
 const CoinsStack = () => {
