3ba6c49e78df3214799fea1e66e6340ac4a6410c
Amstrong <monachelloa@uninorte.edu.co>
2020-12-19 20:25:57 -0500

    Agregando la parte de abajo de iconos

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import {Image} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import CoinsStack from 'cryptoTracker/src/components/coins/CoinsStack';
+import Colors from 'cryptoTracker/src/res/colors';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 // const App: () => React$Node = () => {
+
+const Tabs = createBottomTabNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <CoinsStack />
+      <Tabs.Navigator
+        tabBarOptions={{
+          tintColor: '#fefefe',
+          style: {
+            backgroundColor: Colors.blackPearl,
+          },
+        }}>
+        <Tabs.Screen
+          name="Coins"
+          component={CoinsStack}
+          options={{
+            tabBarIcon: ({size, color}) => (
+              <Image
+                style={{tintColor: color, width: size, height: size}}
+                source={require('cryptoTracker/src/assets/bank.png')}
+              />
+            ),
+          }}
+        />
+      </Tabs.Navigator>
     </NavigationContainer>
   );
 };
